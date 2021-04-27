7f364fa594d3c90b2f4dfb2b40ddb4ad5e186d79
liangzhuang <liangzhuang@aobi.com>
2021-04-27 15:02:38 +0800

    init commit

---
+++ src/main/java/com/no3003/fatlonely/FatlonelyApplication.java
@@ -0,0 +1,13 @@
+package com.no3003.fatlonely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatlonelyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FatlonelyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/no3003/fatlonely/FatlonelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.no3003.fatlonely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatlonelyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
